8553f3e3e072a44a9a5af48b5d94ff01f4d8c2a7
Caio Peres <caioperescs@hotmail.com>
2024-10-29 16:55:50 -0300

    test: add create-product test

--- api/src/modules/products/use-cases/create-product.spec.ts
+++ api/src/modules/products/use-cases/create-product.spec.ts
@@ -1,3 +1,24 @@
-test('2+2 = 4', () => {
-	expect(2 + 2).toBe(4)
+import { InMemoryProductsRepository } from '../repositories/in-memory/in-memory-products.repository.js'
+import { CreateProductUseCase } from './create-product.use-case.js'
+
+let productsRepository: InMemoryProductsRepository
+let sut: CreateProductUseCase
+
+describe('Create Product Use Case', () => {
+	beforeEach(() => {
+		productsRepository = new InMemoryProductsRepository()
+		sut = new CreateProductUseCase(productsRepository)
+	})
+
+	it('should be able to create a new product', async () => {
+		const product = await sut.execute({
+			name: 'Product 1',
+			description: 'Description 1',
+			photo_url: 'https://example.com/photo1.jpg',
+			price: 10,
+		})
+
+		expect(product).toHaveProperty('id')
+		expect(product.id).toEqual(expect.any(String))
+	})
 })
